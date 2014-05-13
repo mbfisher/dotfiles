30c39c3bd1e5e6eddef551081a1f035d1df746e9
Mike Fisher <michael.fisher@bskyb.com>
2014-05-13 14:56:13 +0100

    More fix ohmyzsh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 if [ -z ~/.oh-my-zsh ] ; then
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 else
-    GIT_DIR=$HOME/.oh-my-zsh/.git git pull origin master
+    GIT_DIR=~/.oh-my-zsh/.git git pull origin master
 fi
 
 FILES=`find -type f | grep -v ".git/\|README\|install.sh" | sed 's/^\.\///' | grep "$1"`
