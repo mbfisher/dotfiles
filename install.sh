a835cd1f361ae32927fe9aac2f64f19a7dc77b43
Mike Fisher <mike@mbfisher.com>
2018-03-20 10:05:20 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,13 @@
 if [ ! -d ~/.oh-my-zsh ] ; then
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 else
-    GIT_DIR=~/.oh-my-zsh/.git git pull origin master
+    git -C ~/.oh-my-zsh pull origin master
 fi
 
-if uname -a | grep Darwin >/dev/null; then
-    FIND="find . -type f"
-else
-    FIND="find -type f"
-fi
-
-FILES=`$FIND | grep -v ".git/\|README\|install.sh" | sed 's/^\.\///' | grep "$1"`
-
-for FILE in $FILES ; do
-    echo Installing $FILE
+for FILE in $(find . -type f | grep -v ".git\/\|install.sh\|README" | sed 's/^\.\///'); do
+    echo "Installing $FILE ~/$FILE"
     rm -f ~/$FILE
     mkdir -p ~/`dirname $FILE`
-    echo "$PWD/$FILE ~/$FILE"
     ln -sf $PWD/$FILE ~/$FILE
 done
 
