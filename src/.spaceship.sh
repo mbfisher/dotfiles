4906afc0a6c3e3ebee94c6273e434507d77c1368
Mike Fisher <mike@mbfisher.com>
2018-07-24 11:40:23 +1000

    Add awsauth to prompt

--- src/.spaceship.sh
+++ src/.spaceship.sh
@@ -4,9 +4,30 @@ SPACESHIP_GIT_STATUS_COLOR="yellow"
 SPACESHIP_PROMPT_ORDER=(
   dir
   git
+  awsauth
   exec_time
   line_sep
   jobs
   exit_code
   char
 )
+
+SPACESHIP_AWSAUTH_SHOW="${SPACESHIP_AWSAUTH_SHOW=true}"
+SPACESHIP_AWSAUTH_PREFIX="${SPACESHIP_AWSAUTH_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
+SPACESHIP_AWSAUTH_SUFFIX="${SPACESHIP_AWSAUTH_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
+SPACESHIP_AWSAUTH_SYMBOL="${SPACESHIP_AWSAUTH_SYMBOL="🚀"}"
+SPACESHIP_AWSAUTH_COLOR="${SPACESHIP_AWSAUTH_COLOR="white"}"
+
+spaceship_awsauth() {
+    [[ -n "$AWS_ROLE_IDENTIFIER" ]] || return
+
+    local role=${AWS_ROLE_IDENTIFIER##*/}
+
+    spaceship::section \
+        "$SPACESHIP_AWSAUTH_COLOR" \
+        "$SPACESHIP_AWSAUTH_PREFIX" \
+        "$SPACESHIP_AWSAUTH_SYMBOL $role" \
+        "$SPACESHIP_AWSAUTH_SUFFIX"
+}
+
+function awsauth { bash $HOME/.aws/auth.sh "$@"; [[ -r "$HOME/.aws/sessiontoken" ]] && . "$HOME/.aws/sessiontoken"; }
